bbb8562a951e74c033624e2a7b9917ace8f445fa
Alan Accurso <aaccurso@medallia.com>
2020-05-25 16:09:46 -0300

    chore(bot): configure dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const { Telegraf } = require('telegraf')
+require('dotenv').config();
+const Telegraf = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+const PhotoURL = 'https://picsum.photos/200/300/?random';
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start(ctx => ctx.reply('Hello there!'));
+bot.help(ctx => ctx.reply('Help message'));
+bot.command('photo', ctx => ctx.replyWithPhoto({ url: PhotoURL }));
+bot.launch();
