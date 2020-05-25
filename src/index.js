6840a218ca93e92db294db62f183ce937341adbb
Alan Accurso <aaccurso@medallia.com>
2020-05-25 18:48:36 -0300

    feat(app): add /photo endpoint to post photos to Telegram

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
+// ENV VARIABLES
 require('dotenv').config();
+const {
+	BOT_TOKEN,
+	PORT,
+	// TODO: CHAT_ID will be a list of chat ids persisted in a key-value store.
+	CHAT_ID,
+} = process.env;
+
+// DEPENDENCIES
 const Telegraf = require('telegraf');
+const express = require('express');
 
-const PhotoURL = 'https://picsum.photos/200/300/?random';
+// MULTI PART UPLOAD
+const multer = require('multer');
+const upload = multer();
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// TELEGRAF BOT
+const bot = new Telegraf(BOT_TOKEN);
 bot.start(ctx => ctx.reply('Hello there!'));
 bot.help(ctx => ctx.reply('Help message'));
-bot.command('photo', ctx => ctx.replyWithPhoto({ url: PhotoURL }));
-bot.launch();
+const photo = { url: 'https://picsum.photos/200/300/?random' };
+bot.command('photo', ctx => ctx.replyWithPhoto(photo));
+bot.launch().catch(console.error);
+
+// EXPRESS APP
+const app = express();
+app.post('/photo', upload.single('photo'), (req, res, next) => {
+	const {
+		buffer,
+		originalname,
+	} = req.file;
+
+	bot.telegram.sendPhoto(CHAT_ID, {
+		source: buffer,
+	})
+	.then(() => res.send(`Image "${originalname}" sent!`))
+	.catch(next);
+});
+
+app.listen(PORT, () => {
+	console.log(`Express listening in port ${PORT}`);
+});
